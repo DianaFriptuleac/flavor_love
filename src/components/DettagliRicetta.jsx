1b2024d923c51d40860a6abfbae837d432904066
Diana <dianadorojuc3@gmail.com>
2024-12-07 18:57:03 +0700

    css per dettagliRicette

--- src/components/DettagliRicetta.jsx
+++ src/components/DettagliRicetta.jsx
@@ -2,8 +2,22 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, useNavigate } from "react-router-dom";
 import { fetchDettagliRicetta } from "../redux/actions/fetchRicetteAction";
-import { Container, Card, Button, Alert, ListGroup } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Button,
+  Alert,
+  ListGroup,
+} from "react-bootstrap";
 import { useState } from "react";
+import { FaRegClock, FaTrashAlt, FaEdit, FaShoppingCart } from "react-icons/fa";
+import { PiCookingPotDuotone } from "react-icons/pi";
+import { HiOutlineCurrencyEuro } from "react-icons/hi2";
+import { TbChefHat } from "react-icons/tb";
+
+import "../css/DettagliRicetta.css";
 
 const DettagliRicetta = () => {
   const { id } = useParams(); // ottengo id ricetta
@@ -13,7 +27,10 @@ const DettagliRicetta = () => {
   const { dettagli, loading, error } = useSelector((state) => state.ricette);
   const userId = useSelector((state) => state.auth.user?.id); //id utente
   const token = useSelector((state) => state.auth.token); // token
-  const [notification, setNotification] = useState({ message: "", variant: "" });
+  const [notification, setNotification] = useState({
+    message: "",
+    variant: "",
+  });
 
   const handleDelete = async () => {
     const confermaDelete = window.confirm(
@@ -50,10 +67,13 @@ const DettagliRicetta = () => {
   //aggiungo ricetta alla lista spesa
   const aggiungiAllaListaSpesa = async () => {
     try {
-      const response = await fetch(`http://localhost:3001/api/lista-spesa/${id}`, {
-        method: "POST",
-        headers: { Authorization: `Bearer ${token}` },
-      });
+      const response = await fetch(
+        `http://localhost:3001/api/lista-spesa/${id}`,
+        {
+          method: "POST",
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
 
       if (response.ok) {
         setNotification({
@@ -73,7 +93,6 @@ const DettagliRicetta = () => {
     }
   };
 
-
   useEffect(() => {
     if (id) {
       dispatch(fetchDettagliRicetta(id));
@@ -92,67 +111,128 @@ const DettagliRicetta = () => {
     return <Alert variant="info">Nessuna ricetta trovata.</Alert>;
   }
 
+  // Raggruppo ingredienti per sezione
+  const ingredientiPerSezione = dettagli.ingredienti?.reduce((acc, ing) => {
+    if (!acc[ing.sezione]) {
+      acc[ing.sezione] = [];
+    }
+    acc[ing.sezione].push(ing);
+    return acc;
+  }, {});
+
   return (
-    <Container>
-       {notification.message && <Alert variant={notification.variant}>{notification.message}</Alert>}
+    <div className="bg-dettagliRicetta">
+      <Container className="mt-4 dettagli-ricetta">
+        {notification.message && (
+          <Alert variant={notification.variant}>{notification.message}</Alert>
+        )}
 
-      <Card className="mt-3">
-        <Card.Img
-          variant="top"
-          src={dettagli.img?.[0]?.url || "/assets/default_ricetta.jpg"}
-          alt={dettagli.titolo}
-        />
-        <Card.Body>
-          <Card.Title>{dettagli.titolo}</Card.Title>
-          <Card.Text>
-            <strong>Procedimento:</strong> {dettagli.procedimento}
-          </Card.Text>
-          <Card.Text>
-            <strong>Difficoltà:</strong> {dettagli.difficoltaRicetta}
-          </Card.Text>
-          <Card.Text>
-            <strong>Tempo di Preparazione:</strong>{" "}
-            {dettagli.tempoPreparazioneMinuti} minuti
-          </Card.Text>
-          <Card.Text>
-            <strong>Tempo di Cottura:</strong> {dettagli.tempoCotturaMinuti}{" "}
-            minuti
-          </Card.Text>
-          <Card.Text>
-            <strong>Costo:</strong> {dettagli.costoRicetta}
-          </Card.Text>
-          <strong>Ingredienti:</strong>
-          <ListGroup className="mb-3">
-            {(dettagli.ingredienti || []).map((ing, index) => (
-              <ListGroup.Item key={ing.id}>
-                {ing.nome} {ing.dosaggio}
-              </ListGroup.Item>
-            ))}
-              </ListGroup>
-            <Button variant="success" onClick={aggiungiAllaListaSpesa}>
-            Aggiungi alla Lista Spesa
-            </Button>
-        
+        <Row>
+          {/* Immagine e Titolo */}
+          <Col md={6}>
+            <Card className="shadow-sm border-0">
+              <Card.Img
+                variant="top"
+                src={dettagli.img?.[0]?.url || "/assets/default_ricetta.jpg"}
+                alt={dettagli.titolo}
+                className="rounded"
+              />
+            </Card>
+          </Col>
+          <Col md={6} className="mt-2">
+            <Card className="shadow-sm border-0 p-3">
+              <Card.Body>
+                <Card.Title className="titleCard display-6">
+                  {dettagli.titolo}
+                </Card.Title>
 
-          {dettagli.utente?.id === userId ? (
-            // Mostra solo se e creatore
+                <ListGroup className="list-unstyled">
+                  <li className="d-flex align-items-center">
+                    <FaRegClock className="me-3" />
+                    <strong>Preparazione:</strong>{" "}
+                    {dettagli.tempoPreparazioneMinuti} min
+                  </li>
+                  <li className="d-flex align-items-center">
+                    <PiCookingPotDuotone className="me-3 " />
+                    <strong>Cottura:</strong> {dettagli.tempoCotturaMinuti} min
+                  </li>
+                  <li className="d-flex align-items-center">
+                    <TbChefHat className="me-3" />
+                    <strong>Difficoltà:</strong> {dettagli.difficoltaRicetta}
+                  </li>
+                  <li className="d-flex align-items-center">
+                    <HiOutlineCurrencyEuro className="me-3" />
+                    <strong>Costo:</strong> {dettagli.costoRicetta}
+                  </li>
+                </ListGroup>
+                {dettagli.utente?.id === userId && (
+                  <div className="mt-3 btn-container">
+                    <Button
+                      className="me-3 modifica-ricetta-btn d-flex align-items-center"
+                      onClick={() => navigate(`/ricette/${id}/update`)}
+                    >
+                      <FaEdit className="me-1" /> Modifica
+                    </Button>
+                    <Button
+                      className="me-3 elimina-ricetta-btn d-flex align-items-center"
+                      onClick={handleDelete}
+                    >
+                      <FaTrashAlt className="me-1" /> Elimina
+                    </Button>
+                  </div>
+                )}
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row></Row>
 
-            <div className="d-flex justify-content-between">
+        <Row className="mt-5">
+          <Col>
+            <h2 className="fw-bold ms-1">Ingredienti:</h2>
+            {ingredientiPerSezione &&
+              Object.entries(ingredientiPerSezione).map(
+                ([sezione, ingredienti]) => (
+                  <div key={sezione} className="mb-4">
+                    <h5 className="text-secondary">{sezione}</h5>
+                    <ListGroup>
+                      {ingredienti.map((ing) => (
+                        <ListGroup.Item
+                          key={ing.id}
+                          className="d-flex justify-content-between"
+                        >
+                          <span className="fw-bold ingredienti-nome">
+                            {ing.nome}
+                          </span>
+                          <span>{ing.dosaggio}</span>
+                        </ListGroup.Item>
+                      ))}
+                    </ListGroup>
+                  </div>
+                )
+              )}
+            <div className="d-flex justify-content-center">
               <Button
-                variant="warning"
-                onClick={() => navigate(`/ricette/${id}/update`)}
+                className="shopping-btn d-flex align-items-center"
+                onClick={aggiungiAllaListaSpesa}
               >
-                Modifica
-              </Button>
-              <Button variant="danger" onClick={handleDelete}>
-                Cancella
+                <FaShoppingCart className="me-1" /> Aggiungi alla lista spesa
               </Button>
             </div>
-          ) : null}
-        </Card.Body>
-      </Card>
-    </Container>
+          </Col>
+        </Row>
+
+        <div>
+          {" "}
+          <h2 className="fw-bold mb-0 ms-1">Preparazione:</h2>
+          <Card className="my-3">
+            <Card.Text className="p-3 preparazione">
+              {dettagli.procedimento}
+            </Card.Text>
+          </Card>
+        </div>
+      </Container>
+    </div>
   );
 };
-
 export default DettagliRicetta;
