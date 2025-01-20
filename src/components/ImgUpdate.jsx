dd3974b82bf46d3978ddba79b0a7fc4a4df4c0b7
Diana <dianadorojuc3@gmail.com>
2025-01-20 11:51:52 +0700

    add spinner per modifica ricetta img.

--- src/components/ImgUpdate.jsx
+++ src/components/ImgUpdate.jsx
@@ -1,5 +1,5 @@
-import { Form, Button, ListGroup } from "react-bootstrap";
-import { useEffect } from "react";
+import { Form, Button, ListGroup, Spinner } from "react-bootstrap";
+import { useEffect, useState } from "react";
 import { IoTrash } from "react-icons/io5";
 import { useDispatch, useSelector } from "react-redux";
 import { addUpdateImg, removeImage } from "../redux/actions/updateRicettaActions";
@@ -7,6 +7,7 @@ import "../css/ModificaRicetta.css";
 
 const ImgUpdate = ({ ricettaId }) => {
   const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(false)
 //stato completo
   const state = useSelector((state) => state);
   console.log("IMG Stato completo di Redux:", state);
@@ -21,17 +22,33 @@ const ImgUpdate = ({ ricettaId }) => {
   }, [images]);
 
   
-  const handleImageChange = (e) => {
+  const handleImageChange = async (e) => {
     const file = e.target.files[0];
     if (file) {
       console.log("File selezionato:", file);
-      dispatch(addUpdateImg(ricettaId, file));
+      setIsLoading(true);
+      try{
+        await dispatch(addUpdateImg(ricettaId, file));
+      } catch(err){
+        console.log("Errore durante il carricamento dell'immagine", err);
+      }finally{
+        setIsLoading(false);
+      }
+    
     }
   };
 
   // delete img
-  const handleRemoveImage = (imageId) => {
-    dispatch(removeImage(ricettaId, imageId));
+  const handleRemoveImage = async (imageId) => {
+    setIsLoading(true);
+    try{
+      await dispatch(removeImage(ricettaId, imageId));
+    } catch(err){
+      console.log("Errore durante l'eliminazione dell'immagine", err);
+    }finally{
+      setIsLoading(false);
+    }
+    
   };
 
   return (
@@ -45,9 +62,18 @@ const ImgUpdate = ({ ricettaId }) => {
           className="modificaRicetta-input"
           type="file"
           onChange={handleImageChange}
+          disabled={isLoading} //input disabilitato durante il caricamento
         />
       </Form.Group>
 
+ {/* spinner */}
+ {isLoading && (
+  <div className="d-flex justify-content-center my-3">
+    <Spinner animation="border" role="status">
+      <span className="visually-hidden">Carricamento...</span>
+    </Spinner>
+    </div>
+ )}
       {/* Lista immagini esistenti */}
       <ListGroup className="mt-3">
         {images.length > 0 ? (
@@ -67,6 +93,7 @@ const ImgUpdate = ({ ricettaId }) => {
                 variant="outline-danger"
                 size="sm"
                 onClick={() => handleRemoveImage(img.id)}
+                disabled={isLoading}  //btn disabilitato durante il caricamento
               >
                 <IoTrash />
               </Button>
