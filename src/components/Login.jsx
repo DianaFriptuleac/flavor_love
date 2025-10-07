1f83a73fc8daf38bd4e300ccd32a90c4d2d453d5
Diana <dianadorojuc3@gmail.com>
2025-10-07 16:56:10 +0700

    Aggiunta eye Icon per password, fix css

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,7 @@ import { loginUser } from "../redux/actions/authActions";
 import { Container, Row, Col, Form, Button, Alert } from "react-bootstrap";
 import "../css/Login.css";
 import { useNavigate } from "react-router-dom";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Login = () => {
   const [userCredentials, setUserCredentials] = useState({
@@ -12,6 +13,7 @@ const Login = () => {
   });
   const [message, setMessage] = useState("");
   const [variant, setVariant] = useState("success");
+  const [showPassword, setShowPassword] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
@@ -66,12 +68,14 @@ const Login = () => {
       setVariant("danger");
     }
   };
+  // Icona showPassword
+  const toggleShowPassword = () => setShowPassword((show) => !show);
 
   return (
     <div className="login-background">
       <Container>
         <Row className="justify-content-center">
-          <Col md={6}>
+             <Col md={8} lg={6}>
             <h1 className="text-light mb-0 text-center mt-2 loginTitle">
               Fai il Login
             </h1>
@@ -88,17 +92,25 @@ const Login = () => {
                   required
                 />
               </Form.Group>
-              <Form.Group controlId="formPassword" className="mb-3">
+              <Form.Group controlId="formPassword" className="mb-3 position-relative">
                 <Form.Label className="text-light">Password</Form.Label>
                 <Form.Control
                   className="loginControl"
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                   name="password"
                   value={userCredentials.password}
                   onChange={handleChange}
                   placeholder="Inserisci la tua password"
                   required
                 />
+                <span
+                className="password-toggle-icon"
+                onClick={toggleShowPassword}
+                role="button"
+                aria-label={showPassword ? "Nascondi password" : "Mostra password"}
+                >
+                  {showPassword ? <FaEyeSlash /> : <FaEye /> }
+                </span>
               </Form.Group>
               <div className="d-flex justify-content-end">
                 <Button type="submit" className="w-25 mt-3 loginButton">
